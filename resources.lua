0a824e5f76565a2380ef7fd47ebc60f49d27201c
Simon Larsen <simonhffh@gmail.com>
2016-06-23 03:08:33 +0200

    Added font loading

--- resources.lua
+++ resources.lua
@@ -3,6 +3,7 @@ local serialize = require("prox.serialize")
 local resources = {}
 
 local images = {}
+local fonts = {}
 
 function resources.getImage(path)
 	if images[path] == nil then
@@ -18,6 +19,15 @@ end
 
 function resources.getAnimator(path)
 	return serialize.read(path)
+end
+
+function resources.getFont(path, size)
+	local key = path .. "$" .. size
+	if fonts[key] == nil then
+		fonts[key] = love.graphics.newFont(path, size)
+	end
+
+	return fonts[key]
 end
 
 return resources

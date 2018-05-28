dd38d55e49975c15f90a556e971794941a015fad
Simon Larsen <simonhffh@gmail.com>
2018-05-28 18:12:36 +0200

    Updated for LÖVE 11.0

--- resources.lua
+++ resources.lua
@@ -1,17 +1,19 @@
 local serialize = require("prox.serialize")
 
-local resources = {}
-
 local images = {}
 local fonts = {}
 local sounds = {}
 local shaders = {}
 
-resources.exists = love.filesystem.exists
+local resources = {}
+
+function resources.exists(path)
+    return love.filesystem.getInfo(path) ~= nil
+end
 
 function resources.getImage(path)
 	if images[path] == nil then
-		assert(love.filesystem.exists(path), "Image file \"" .. path .. "\" not found.")
+		assert(resources.exists(path), "Image file \"" .. path .. "\" not found.")
 		images[path] = love.graphics.newImage(path)
 	end
 
@@ -19,19 +21,19 @@ function resources.getImage(path)
 end
 
 function resources.getAnimation(path)
-	assert(love.filesystem.exists(path), "Animation file \"" .. path .. "\" not found.")
+	assert(resources.exists(path), "Animation file \"" .. path .. "\" not found.")
 	return serialize.read(path)
 end
 
 function resources.getAnimator(path)
-	assert(love.filesystem.exists(path), "Animator file \"" .. path .. "\" not found.")
+	assert(resources.exists(path), "Animator file \"" .. path .. "\" not found.")
 	return serialize.read(path)
 end
 
 function resources.getFont(path, size)
 	local key = path .. "$" .. size
 	if fonts[key] == nil then
-		assert(love.filesystem.exists(path), "Font file \"" .. path .. "\" not found.")
+		assert(resources.exists(path), "Font file \"" .. path .. "\" not found.")
 		fonts[key] = love.graphics.newFont(path, size)
 	end
 
@@ -42,7 +44,7 @@ resources.setFont = love.graphics.setFont
 
 function resources.getImageFont(path, glyphs, spacing)
 	if fonts[path] == nil then
-		assert(love.filesystem.exists(path), "Image font file \"" .. path .. "\" not found.")
+		assert(resources.exists(path), "Image font file \"" .. path .. "\" not found.")
 		fonts[path] = love.graphics.newImageFont(path, glyphs, spacing or 2)
 	end
 	return fonts[path]
@@ -50,7 +52,7 @@ end
 
 function resources.getSound(path)
 	if sounds[path] == nil then
-		assert(love.filesystem.exists(path), "Sound file \"" .. path .. "\" not found.")
+		assert(resources.exists(path), "Sound file \"" .. path .. "\" not found.")
 		sounds[path] = love.audio.newSource(path, "static")
 	end
 	return sounds[path]
@@ -58,7 +60,7 @@ end
 
 function resources.getShader(path)
 	if shaders[path] == nil then
-		assert(love.filesystem.exists(path), "Shader file \"" .. path .. "\" not found.")
+		assert(resources.exists(path), "Shader file \"" .. path .. "\" not found.")
 		shaders[path] = love.graphics.newShader(path)
 	end
 	return shaders[path]
